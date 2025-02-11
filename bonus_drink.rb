abbf152a2c9e3260869776ed91e20402a6aea251
satoharu-reimon <dreimon47@gmail.com>
2025-02-11 17:39:55 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -2,7 +2,7 @@ class BonusDrink
   def self.total_count_for(amount){
      sum = amount;   
      empty = amount;
-     while(enpty>=3){
+     while(empty>=3){
        sum += empty/3;
        empty = empty/3 + empty%3;
        }
