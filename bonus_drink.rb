8d53fb6a65602d75db5bb1211a1add6adb4f9e2f
satoharu-reimon <dreimon47@gmail.com>
2025-02-11 19:16:32 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -3,8 +3,8 @@ class BonusDrink
      sum = amount;
      new = amount;
      while(new>=3)
-       sum += new/3;  //新しい飲み物を合計する
-       new = new/3 + new%3;  //新しい飲み物とあまり
+       sum += new/3;
+       new = new/3 + new%3;
        end
       sum;
      end
