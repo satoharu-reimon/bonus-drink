6b99b5d547ba188ccff0e71e8f08bc2d4590c75c
satoharu-reimon <dreimon47@gmail.com>
2025-02-11 17:34:02 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,13 +1,12 @@
 class BonusDrink
-   remainder_quotient(n){
-    (n/3 + n%3)/3
-    }
   def self.total_count_for(amount){
-    sum = amount + amount/3,
-    while(remainder_quotient(n)){
-      sum + remainder_quotient(remainder_quotient(n))
-    }
-      total_count_for = sum
-    }
+     sum = amount;   
+     empty = amount;
+     while(enpty>=3){
+       sum += empty/3;
+       enpty = empty/3 + empty%3;
+       }
+      sum;
+   }
   end
 end
